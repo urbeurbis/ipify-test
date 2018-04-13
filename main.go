f6e6a3a083fc58cfef5559201748111351d9058c
root <root@linuxunix>
2018-04-13 22:13:17 +0100

    second commit

--- main.go
+++ main.go
@@ -5,6 +5,69 @@
 // ipify is a simple API service which returns a user's public IP address (it
 // supports handling both IPv4 and IPv6 addresses).
 
+
+
+
+
+
+
+
+
+
+
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	addr     = flag.String("addr", ":3000", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+
+if string(ctx.RequestURI()[:]) == "/" {
+
+	fmt.Fprintf(ctx, ctx.RemoteIP().String())
+}
+
+	ctx.SetContentType("text/plain; charset=utf8")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 package main
 
 import (
@@ -41,3 +104,5 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, handler))
 
 }
+
+*/
